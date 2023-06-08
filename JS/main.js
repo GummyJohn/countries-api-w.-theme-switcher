007755069de9e2a79342451e431aec2f3a27888e
Johnny <john@Richies-iMac.local>
2023-06-08 14:49:26 -0400

    working on requestes

--- JS/main.js
+++ JS/main.js
@@ -28,3 +28,37 @@ header.append(darkModeBtn);
 body.append(header);
 body.append(grayBackground);
 
+const inputSelectContainer = divCreate('input-dropdown');
+
+const searchInputContainer = divCreate('search-input-container')
+searchInputContainer.innerHTML = 
+`<button class='search-img'><i class="fa-solid fa-magnifying-glass"></i></button>`
+
+const searchInput = document.createElement('input');
+searchInput.className= 'input-country';
+searchInput.placeholder = `Search for a country...`;
+
+const dropdown = document.createElement('select');
+dropdown.className = 'dropdown';
+//drop options 
+let countryArr = ['Filter by Region', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+
+countryArr.forEach(country => {
+  const option = document.createElement('option');
+  option.value = country;
+  option.innerText = country;
+
+  dropdown.append(option)
+})
+
+searchInputContainer.append(searchInput);
+inputSelectContainer.append(searchInputContainer);
+inputSelectContainer.append(dropdown);
+
+grayBackground.append(inputSelectContainer);
+
+const countriesDivContainer = divCreate('countries-divs-container');
+
+grayBackground.append(countriesDivContainer)
+
+
