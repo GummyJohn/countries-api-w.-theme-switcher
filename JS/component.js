c4b7472515f5ff39bbd2422f1ef3b7897241109a
Johnny <john@Richies-iMac.local>
2023-06-14 14:32:54 -0400

    clean up

--- JS/component.js
+++ JS/component.js
@@ -1,4 +1,4 @@
-export function divCreate(name){
+function divCreate(name){
   const div = document.createElement('div')
   div.className = name;
 
@@ -13,7 +13,6 @@ function PCreate(name, text){
   return p;
 }
 
-
 function button(name, text){
   const button = document.createElement('button');
   button.className = name;
@@ -22,60 +21,77 @@ function button(name, text){
   return button
 }
 
-
- export const body = document.querySelector('body');
-
-const header = divCreate('header');
-
-const darkModeBtn = button('dark-mode', '');
-darkModeBtn.innerHTML = `<span class='moon'><i class="fa-regular fa-moon"></i></span> Dark Mode`;
-
-header.append(PCreate('title', 'Where in the World?'))
-header.append(darkModeBtn);
-
-body.append(header);
-
-
-const inputSelectContainer = divCreate('input-dropdown');
-
-const searchInputContainer = divCreate('search-input-container')
-searchInputContainer.innerHTML = 
-`<button class='search-img'> 
-  <i class="fa-solid fa-magnifying-glass"></i>
-</button>`
-
-const searchInput = document.createElement('input');
-searchInput.className= 'input-country';
-searchInput.placeholder = `Search for a country...`;
-
-const dropdown = document.createElement('select');
-dropdown.className = 'dropdown';
-
-const firstOption = document.createElement('option');
-firstOption.innerText = 'All'
-
-dropdown.prepend(firstOption);
-
-//drop options 
-let countryArr = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
-
-for(let i = 0; i < countryArr.length; i++){
-  const option = document.createElement('option');
-  option.innerText = countryArr[i];
-  option.value = countryArr[i];
+export function createHeader(){  
+  const header = divCreate('header');
   
-  dropdown.append(option)
+  const darkModeBtn = button('dark-mode', '');
+  darkModeBtn.innerHTML = `<span class='moon'><i class="fa-regular fa-moon"></i></span> Dark Mode`;
+  
+  header.append(PCreate('title', 'Where in the World?'))
+  header.append(darkModeBtn);
+
+  return header
 }
 
+export function createSearchDropdown(){
+  const inputSelectContainer = divCreate('input-dropdown');
+  
+  const searchInputContainer = divCreate('search-input-container')
+  searchInputContainer.innerHTML = 
+  `<button class='search-img'> 
+    <i class="fa-solid fa-magnifying-glass"></i>
+  </button>`
+  
+  const searchInput = document.createElement('input');
+  searchInput.className= 'input-country';
+  searchInput.placeholder = `Search for a country...`;
+  
+  const dropdown = document.createElement('select');
+  dropdown.className = 'dropdown';
+  
+  const firstOption = document.createElement('option');
+  firstOption.innerText = 'All'
+  
+  dropdown.prepend(firstOption);
+  
+  //drop options 
+  let countryArr = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  
+  for(let i = 0; i < countryArr.length; i++){
+    const option = document.createElement('option');
+    option.innerText = countryArr[i];
+    option.value = countryArr[i];
+    
+    dropdown.append(option)
+  }
+  
+  searchInputContainer.append(searchInput);
+  inputSelectContainer.append(searchInputContainer);
+  inputSelectContainer.append(dropdown);
 
-searchInputContainer.append(searchInput);
-inputSelectContainer.append(searchInputContainer);
-inputSelectContainer.append(dropdown);
+  return inputSelectContainer;
+}
 
-body.append(inputSelectContainer);
+export function createDivDisplay(){
+  const countriesDivContainer = divCreate('countries-divs-container');
 
-const countriesDivContainer = divCreate('countries-divs-container');
+  return countriesDivContainer
+}
 
-body.append(countriesDivContainer)
+export function createSingleBox({flags, population, region, capital, name}){
+  const singleDivs = divCreate('single-container')
+  singleDivs.innerHTML=`
+    <img src ='${flags.png}' alt ='${flags.alt}' class='flag-img'>
+    
+    <div class='country-content'>
+      <h2 class= 'country-title'>${name.common}</h2>
+      
+      <p class='population stats'>Population: ${population}</p>
+      <p class='region stats'>Region: ${region}</p>
+      <p class='captial stats'>Captial: ${capital}</p>         
+    </div>
+  `
+  return singleDivs;
+}
 
 
